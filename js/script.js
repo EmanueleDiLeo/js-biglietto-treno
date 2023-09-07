a46dd9be81eca83d69965d2453c12ed97fe1ae14
EmanueleDiLeo <dileoemanuele11@gmail.com>
2023-09-07 19:14:49 +0200

    [ADD] : check < 0

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const kilometres = parseInt(prompt("Scrivi i chilometro da percorrere"));
+const kilometres = parseFloat(prompt("Scrivi i chilometro da percorrere"));
 const age = parseInt(prompt("Scrivi la tua età"));
 let valid = true;
 let message;
@@ -11,15 +11,14 @@ if((isNaN(age || kilometres)) || age==null || kilometres==null || age < 0 || kil
 if(valid){
   let price = kilometres * 0.21;
   if(age < 18){
-    message = "Il prezzo da pagare è " + Math.round((price - (price * 20 / 100)) * 100 ) / 100 + " €"
+    message = "Il prezzo da pagare è " + (price - (price * 20 / 100)).toFixed(2) + " €"
   }
   else if(age > 65){
-    message = "Il prezzo da pagare è " + Math.round((price - (price * 40 / 100)) * 100 ) / 100 + " €"
+    message = "Il prezzo da pagare è " + (price - (price * 40 / 100)).toFixed(2) + " €"
   }
   else{
     message = "Il prezzo da pagare è " + price.toFixed(2) + " €"
   }
 }
-
 
 document.getElementById("output").innerHTML = message;
